3b784cbc8994ba85c95d5ae13d1c2439d3f15d2f
celedituro <cdituro@fi.uba.ar>
2023-05-25 11:23:20 -0300

    refactor: style

--- src/views/WelcomeView.tsx
+++ src/views/WelcomeView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/WelcomeView.css';
 import Header from '../components/Header';
+import Button from '@mui/material/Button';
 
 const WelcomeView: React.FC = () => {
   return (
@@ -9,7 +10,7 @@ const WelcomeView: React.FC = () => {
       <div className="welcome-view">
         <h1>Bienvenido a Careerwise</h1>
         <p>Descubre cuál es tu vocación y encuentra el camino hacia tu futuro profesional</p>
-        <button type="submit" className="start-button">Comenzar</button>
+        <Button color="secondary" variant="contained">Comenzar</Button>
       </div>
     </>
   );
