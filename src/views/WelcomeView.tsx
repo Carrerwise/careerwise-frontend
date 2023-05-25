4843bb214a50a80485211b18ee5a86f790fc9102
celedituro <cdituro@fi.uba.ar>
2023-05-25 12:14:06 -0300

    feat: add card to start test

--- src/views/WelcomeView.tsx
+++ src/views/WelcomeView.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/WelcomeView.css';
 import Header from '../components/Header';
 import Button from '@mui/material/Button';
+import { Card, CardHeader, CardBody, CardFooter, Heading, Text } from '@chakra-ui/react';
 
 const WelcomeView: React.FC = () => {
   return (
@@ -9,8 +10,17 @@ const WelcomeView: React.FC = () => {
       <Header />
       <div className="welcome-view">
         <h1>Bienvenido a Careerwise</h1>
-        <p>Descubre cuál es tu vocación y encuentra el camino hacia tu futuro profesional</p>
-        <Button color="secondary" variant="contained">Comenzar</Button>
+        <Card align="center" className="main-card">
+          <CardHeader>
+            <Heading> Test vocacional</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>Descubre cuál es tu vocación y encuentra el camino hacia tu futuro profesional</Text>
+          </CardBody>
+          <CardFooter>
+            <Button color="secondary" variant="contained">Comenzar</Button>
+          </CardFooter>
+        </Card>
       </div>
     </>
   );
