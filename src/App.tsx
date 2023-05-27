a4bcf7f6397e002cdcc5fcaad50cb92e7d5b195c
celedituro <cdituro@fi.uba.ar>
2023-05-27 12:33:52 -0300

    feat: add sourvey results view

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomeView />} />
         <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/results" element={<SurveyView />} />
       </Routes>
     </Router>
   );
