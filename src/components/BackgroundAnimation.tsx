b8697c0940f00375e9be2c121c73640eb45098e4
celedituro <cdituro@fi.uba.ar>
2023-06-02 14:25:56 -0300

    feat: add animation to welcome view

---
+++ src/components/BackgroundAnimation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ParticlesBg from 'particles-bg';
+
+const BackgroundAnimation = () => {
+  return (
+    <ParticlesBg type="tadpole" num={80} bg={true} />
+  );
+};
+
+export default BackgroundAnimation;
--- src/views/WelcomeView.tsx
+++ src/views/WelcomeView.tsx
@@ -5,12 +5,14 @@ import { Card, CardHeader, CardBody, CardFooter, Heading, Text } from '@chakra-u
 
 import '../styles/WelcomeView.css';
 import '../styles/Global.css'
+import BackgroundAnimation from '../components/BackgroundAnimation';
 
 const WelcomeView: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <div className="welcome-container">
+      <BackgroundAnimation />
       <div className="welcome-view">
         <h1 className="welcome-title">Bienvenido a Careerwise</h1>
         <Card align="center" className="main-card">
