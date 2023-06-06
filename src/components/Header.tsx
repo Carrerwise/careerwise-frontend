b194608d47983074cd96fac5954af98ccb13f1af
celedituro <cdituro@fi.uba.ar>
2023-06-05 23:13:59 -0300

    feat: add link to home in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,14 @@
 import '../styles/Header.scss'
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
 
   return (
     <header className="header_container">
-      <h1 className="header_title">Careerwise</h1>
-      {/* <div className="header_link_container">
-        <a className={`header_link ${location.pathname === "/" && "header_link_active"}`} href="/">
-          Inicio
-        </a>
-      </div> */}
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <h1 className="header_title">Careerwise</h1>
+      </Link>
     </header>
   );
 }
