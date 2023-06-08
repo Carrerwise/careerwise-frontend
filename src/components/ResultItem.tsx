2f8e0ebc0d36151004654995d0a16a823be4a88b
alejovillores <avillores@fi.uba.ar>
2023-06-08 12:54:56 -0300

    feat: emojis

--- src/components/ResultItem.tsx
+++ src/components/ResultItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text, Heading, Grid, GridItem, Center } from '@chakra-ui/react';
+import { Box, Text, Heading, Grid, GridItem, Center,ListItem,UnorderedList} from '@chakra-ui/react';
 import '../styles/TestResults.css';
 
 type ResultItemProps = {
@@ -8,22 +8,31 @@ type ResultItemProps = {
 }
  
 const ResultItem = ({ prop, label }: ResultItemProps) => {
+
     return (
         <Box className="resultItem" _hover={{
             background: "#ededed",
             }}
         >
             <Heading as="h3" size="lg" mb={2} fontWeight={"normal"} color={"#3c3b3d"}>
-              {label}
+                💡{label}
             </Heading>
             <Grid>
                 <GridItem key={prop.name}>
                     <Center p={2} bg="gray.200" borderRadius="md">
                         <Text>{prop.aptitude}{prop.activity}{prop.name}</Text>
-                        <Text>{prop.institution}</Text>
-                        <Text>{prop.city}</Text>
-                        <Text>{prop.type}</Text>
-                        <Text>{prop.academy_level}</Text>
+                        {
+                            label == 'Carrera' &&
+                            <>
+                            <UnorderedList>
+                                <ListItem><Text> {'🏫 ' + prop.institution}</Text></ListItem>
+                                <ListItem><Text> {'📍 ' + prop.city}</Text></ListItem>
+                                <ListItem><Text> {'⚡️ ' + prop.type}</Text></ListItem>
+                                <ListItem><Text> {'📃 ' + prop.academy_level}</Text></ListItem>
+                            </UnorderedList>
+                            </>
+                        }
+
                     </Center>
                 </GridItem>
             </Grid>
